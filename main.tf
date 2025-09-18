fb5acb0c153589a9dc0056bb2a639fa2c518f4e4
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-09-18 16:25:38 +0530

    Adding port

--- main.tf
+++ main.tf
@@ -112,6 +112,14 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "TFE Console"
+    from_port   = 8800
+    to_port     = 8800
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
